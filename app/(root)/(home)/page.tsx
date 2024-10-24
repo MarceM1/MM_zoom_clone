588cf09aba713c928961d4411282af54523682f1
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-10-24 15:31:46 -0300

    Fix Upcomingmeeting Alert

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,47 +1,51 @@
 'use client'
 
 import MeetingTypeList from '@/components/MeetingTypeList';
+import NextMeeting from '@/components/NextMeeting';
 import React, { useEffect, useState } from 'react'
 
 const Home = () => {
-  
-    const [currentTime, setCurrentTime] = useState({
-      time: '',
-      date: ''
-    });
 
-    const {time , date}= currentTime
-  
-    useEffect(() => {
-      const updateTime = () => {
-        const now = new Date();
-        const time = now.toLocaleTimeString('en-US', {
-          hour: '2-digit',
-          minute: '2-digit',
-        });
-        const date = new Intl.DateTimeFormat('en-US', {
-          dateStyle: 'full',
-        }).format(now);
-  
-        setCurrentTime({ time, date });
-      };
-  
-      // Actualizar la hora al cargar la página
-      updateTime();
-  
-      // Establecer un intervalo para actualizar cada minuto (60000 ms)
-      const intervalId = setInterval(updateTime, 3000);
-  
-      // Limpiar el intervalo cuando el componente se desmonte
-      return () => clearInterval(intervalId);
-    }, []);
-    
+  const [currentTime, setCurrentTime] = useState({
+    time: '',
+    date: ''
+  });
+
+  const { time, date } = currentTime
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const time = now.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      const date = new Intl.DateTimeFormat('en-US', {
+        dateStyle: 'full',
+      }).format(now);
+
+      setCurrentTime({ time, date });
+    };
+
+    // Actualizar la hora al cargar la página
+    updateTime();
+
+    // Establecer un intervalo para actualizar cada minuto (60000 ms)
+    const intervalId = setInterval(updateTime, 3000);
+
+    // Limpiar el intervalo cuando el componente se desmonte
+    return () => clearInterval(intervalId);
+  }, []);
+
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meeting at: 12:30 PM</h2>
+          <div className='glassmorphism max-w-[400px] rounded py-2 items-center justify-center text-base font-normal flex gap-2'>
+            <h2 className='text-base font-normal'>Upcoming Meeting at: </h2>
+            <NextMeeting type='upcoming' />
+          </div>
           <div className="flex flex-col gap2">
             <h1 className='text-4xl font-extrabold lg:text-7xl'>
               {time}
@@ -49,7 +53,7 @@ const Home = () => {
             <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
           </div>
         </div>
-      </div> 
+      </div>
 
       <MeetingTypeList />
     </section>
---
+++ components/NextMeeting.tsx
@@ -0,0 +1,89 @@
+// @ts-nocheck
+
+'use client'
+import { useGetCalls } from '@/hooks/useGetCalls'
+
+import {  CallRecording } from '@stream-io/video-react-sdk'
+// import { useRouter } from 'next/navigation'
+import React, { useState, useEffect } from 'react'
+import Loader from './Loader'
+import { useToast } from './ui/use-toast'
+
+
+const NextMeeting = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
+    const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls()
+    // const router = useRouter();
+    const [recordings, setRecordings] = useState<CallRecording[]>([])
+
+    const { toast } = useToast();
+
+    const getCalls = () => {
+        switch (type) {
+            case 'ended':
+                return endedCalls
+
+            case 'recordings':
+                return recordings
+
+            case 'upcoming':
+                return upcomingCalls
+
+            default:
+                return ['empty'];
+        }
+    }
+
+    // const getNoCallsMessage = () => {
+    //     switch (type) {
+    //         case 'ended':
+    //             return 'No Previous Calls'
+
+    //         case 'recordings':
+    //             return 'No Recordings'
+
+    //         case 'upcoming':
+    //             return 'No Upcomings Calls'
+
+    //         default:
+    //             return '';
+    //     }
+    // }
+
+    useEffect(() => {
+        const fetchRecordings = async () => {
+            try {
+                const callData = await Promise.all(callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [])
+                const recordings = callData
+                    .filter(call => call.recordings.length > 0)
+                    .flatMap(call => call.recordings)
+
+
+                setRecordings(recordings)
+            } catch (error) {
+                console.log(error)
+                toast({
+                    title: 'Try again later'
+                })
+            }
+        }
+
+        if (type === 'recordings') { fetchRecordings() };
+    }, [type, callRecordings])
+
+    if (isLoading) return <Loader />
+
+    const calls = getCalls();
+    // const noCallsMessage = getNoCallsMessage();
+
+
+    // console.log('calls:', calls)
+    return (
+
+        <h2 className='text-base font-normal'>{
+            calls && calls.length > 0 ? calls[0].state?.startsAt.toLocaleString() || calls[0].start_time?.toLocaleString() : 'All clean!'
+        }</h2>
+
+    )
+}
+
+export default NextMeeting
