27fb579ff3a7079301599475acc176b370b5372a
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-10-25 03:11:55 -0300

    fix

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -70,6 +70,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 		}
 
 		if (type === 'recordings') { fetchRecordings() };
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [type, callRecordings])
 
 	if (isLoading) return <Loader />
